aab863919e613e24f0d1a5cee35fd05b35622908
RahidAmin <rahidamin19@gmail.com>
2024-06-30 00:47:40 +0600

    Navbar added

--- src/Component/Basics/MenuCard.js
+++ src/Component/Basics/MenuCard.js
@@ -1,26 +1,27 @@
 import React from 'react'
 
 const MenuCard = ({ menuData }) => {
-    console.log(menuData)
+
     return (
         <>
             <section className='main-card--cointainer'>
                 {menuData.map((curElement) => {
+                    const { id, name, category, image, description } = curElement;
                     return (
                         <>
 
-                            <div className='card-container' key={curElement.id}>
+                            <div className='card-container' key={id}>
                                 <div className='card'>
                                     <div className='card-body'>
-                                        <span className='card-number cart card-circle subtle'>{curElement.id}</span>
+                                        <span className='card-number cart card-circle subtle'>{id}</span>
                                         <span className='card-author subtle'>{curElement.name}</span>
-                                        <h2 className='card-title'>{curElement.name}</h2>
+                                        <h2 className='card-title'>{name}</h2>
                                         <span className='card-description subtle'>
-                                            {curElement.description}
+                                            {description}
                                         </span>
                                         <div className='card-read'>Read</div>
                                     </div>
-                                    <img src={curElement.image} alt="images" className='card-media' />
+                                    <img src={image} alt="images" className='card-media' />
                                     <span className='card-tag subtle'>Order Now</span>
                                 </div>
                             </div>
--- src/Component/Basics/Restaurant.js
+++ src/Component/Basics/Restaurant.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react'
 import "./style.css"
 import Menu from './menuApi'
-import ManuCard from './MenuCard'
 import MenuCard from './MenuCard'
+import Navbar from './Navbar'
 
+
+
+const uniqueList = [...new Set(Menu.map((curElement) => {
+    return curElement.category;
+})), "All",]
+console.log(uniqueList)
 
 
 const Restaurant = () => {
-    const [menuData, setManuData] = useState(Menu);
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, SetMenuList] = useState(uniqueList);
 
-
-
+    const filterItem = (category) => {
+        if (category === 'All') {
+            setMenuData(Menu);
+            return;
+        }
+        const updatedList = Menu.filter((curElement) => {
+            return curElement.category === category;
+        })
+        setMenuData(updatedList);
+    }
     return (
         <>
-            <nav className='navbar'>
-                <div className='btn-group'>
-                    <button className='btn-group__item'>Breakfast</button>
-                    <button className='btn-group__item'>Lunch</button>
-                    <button className='btn-group__item'>Evening</button>
-                    <button className='btn-group__item'>Dinner</button>
-                    <button className='btn-group__item'>All</button>
-                </div>
-            </nav>
+            <Navbar filterItem={filterItem} menuList={menuList}></Navbar>
             <MenuCard menuData={menuData}></MenuCard>
 
         </>
